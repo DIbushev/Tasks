20bdb9172dc19be882b6fcd1250a9bd996746750
jurasikasan <jurasikasan@speedflow.com>
2018-06-30 12:33:30 +0300

    null safe (optional)

--- reverser/src/main/java/reverse/App.java
+++ reverser/src/main/java/reverse/App.java
@@ -4,10 +4,15 @@ public class App {
 
     public static void main(String[] args) {
         String example = "String to be reversed";
-        System.out.println("Reversed :>"+reverse(example)+"<");
+        System.out.println("Reversed :>" + reverse(example) + "<");
+        System.out.println("Reversed :>" + reverse("") + "<");
+        System.out.println("Reversed :>" + reverse(null) + "<");
     }
 
     private static String reverse(String toBeReversed) {
+        if (toBeReversed == null) {
+            return null;
+        }
         String inverted = "";
         for (int i = toBeReversed.length() - 1; i >= 0; i--) {
             inverted += toBeReversed.charAt(i) + "";
